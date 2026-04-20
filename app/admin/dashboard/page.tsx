d1526d34dd55a53b65ac0c215a8e6cc75bd4d387
Arnold Idiong <idiong.arnold@stu.cu.edu.ng>
2026-04-20 09:44:40 +0100

    new

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -86,7 +86,7 @@ export default function AdminDashboard() {
               ...order,
               disputeClaimedById: String(user?.uid || ''),
               disputeClaimedByEmail: String(user?.email || ''),
-              disputeClaimedByName: String(user?.name || user?.email || ''),
+              disputeClaimedByName: String(user?.displayName || user?.email || ''),
               disputeClaimedAt: new Date().toISOString(),
             }
           : order
